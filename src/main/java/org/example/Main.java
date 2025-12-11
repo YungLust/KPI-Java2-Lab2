32747b95bbf5af39abe587ff1db7d354f78ea39b
YungLust <artyr.yanchuk2@gmail.com>
2025-12-11 20:12:11 +0200

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int studentId = 3429;
+        System.out.println("C4 = "+studentId%4);
+        System.out.println("Decided to change my variant to C4 = 3;");
+        System.out.println("I believe it is more interesting, hope you understand.");
+    }
+}
---
+++ src/main/java/org/example/Undead.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Undead {
+    private String name;
+    private String type;
+    private String domain;
+    private LocalDate appearanceDate;
+    private int attackDamage;
+}
